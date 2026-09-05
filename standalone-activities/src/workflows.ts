03fd8f9868756c00c1ccb10dc8d1a1e09e4a9454
WinZawOo <winzawoo.dev@gmail.com>
2026-09-06 01:38:07 +0800

    chore: create workflow with activities function

---
+++ standalone-activities/src/workflows.ts
@@ -0,0 +1,12 @@
+import { proxyActivities } from "@temporalio/workflow";
+import type * as activities from "./activities.js"
+
+const {greet} = proxyActivities<typeof activities>({
+    startToCloseTimeout: "1 minute",
+})
+
+
+export async function greetingWorkflow(name: string): Promise<string> {
+    return await greet(name);
+}
+
